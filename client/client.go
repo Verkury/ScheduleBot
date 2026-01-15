d832f8d862e4444872aea047fc9d87eb2347729f
Verkury <aleksandrlaty78@gmail.com>
2026-01-15 23:06:22 +0700

    Add func getToken

--- client/client.go
+++ client/client.go
@@ -1,9 +1,46 @@
 package client
 
 import (
+	"fmt"
+	"regexp"
 
+	"github.com/PuerkitoBio/goquery"
 )
 
 func StartClient() {
+	baseUrl := "https://www.asu.ru/timetable/students/15/2129441959/"
+	token, err := getToken(baseUrl)
+	if err != nil {
+		fmt.Println("Err:", err)
+	}
+	getSchedule(baseUrl, token)
+
+}
+
+func getToken(url string) (string, error) {
+    doc, err := goquery.NewDocument(url)
+    if err != nil {
+        return "", err
+    }
+    
+    var token string
+    re := regexp.MustCompile(`'X-CS-ID',\s*'([^']+)'`)
+
+    doc.Find("script").Each(func(i int, s *goquery.Selection) {
+        if token != "" { return }
+        
+        matches := re.FindStringSubmatch(s.Text())
+        if len(matches) > 1 {
+            token = matches[1]
+        }
+    })
+
+    if token == "" {
+        return "", fmt.Errorf("token not found")
+    }
+    return token, nil
+}
+
+func getSchedule(url, token string) {
 
 }
